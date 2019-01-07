5464b47a6c165eb3fa3b39542ea166613a364fce
Jacob Errington <git@mail.jerrington.me>
2019-01-07 07:52:30 -0500

    comments

--- kernel/prepare-disk-image.sh
+++ kernel/prepare-disk-image.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+# prepare a loopback device for disk.img
 DEV=$(losetup --find --partscan --show disk.img)
+# format the EFI system partition as FAT32
 mkfs.fat -F 32 ${DEV}p1
+# mount it and copy the tetris EFI application
+mkdir -p mnt
 mount ${DEV}p1 mnt
 cp tetris.efi mnt
 umount mnt
 sync
+# remove the loopback device
 losetup -d $DEV
